f1bb421fdc138c777424ca28c51cf2a85aa0253e
Darryl Ng <darryl1975@hotmail.com>
2023-02-01 12:52:14 +0800

    initial commit

---
+++ src/main/java/sg/edu/nus/iss/day22_workshop/Day22WorkshopApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day22_workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day22WorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day22WorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/day22_workshop/Day22WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day22_workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day22WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
